59c049444d32d301d5c9018e148053fa72c66904
Olena Boldysheva <76661298+adv555@users.noreply.github.com>
2022-01-06 15:19:01 +0200

    added filtering contacts by favorite & pagination

--- controllers/contacts/getContactsList.js
+++ controllers/contacts/getContactsList.js
@@ -6,12 +6,20 @@ const getContactsList = async (req, res, next) => {
 
     const skip = (page - 1) * limit
     const { _id } = req.user
-    const contacts = await Contact.find({ owner: _id }, '-createdAt -updatedAt', {
+
+    const contacts = await Contact.find({ owner: _id, ...req.query }, '-createdAt -updatedAt', {
       skip,
       limit: +limit,
     })
 
-    res.json({ status: 'success', code: 200, data: { contacts }, skip, limit })
+    res.json({
+      status: 'success',
+      code: 200,
+      data: { contacts },
+      skip,
+      limit,
+      total: contacts.length,
+    })
   } catch (error) {
     next(error)
   }
